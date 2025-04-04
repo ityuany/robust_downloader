75c1936a7c8dbf941c6db7df2ce2916096e09dec
ityuany <519495771@qq.com>
2025-04-04 14:05:39 +0800

    重构下载逻辑，移除不必要的字段，优化DownloadProgress和DownloadState结构体。将HTTP客户端和进度条的创建移至download方法中，增强代码可读性和可维护性。

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env, sync::Arc, time::Duration};
+use std::{env, path::PathBuf, sync::Arc, time::Duration};
 
 use backoff::ExponentialBackoff;
 use futures_util::StreamExt;
@@ -13,16 +13,6 @@ mod state;
 pub struct DownloadProgress {
     #[builder(default = Duration::from_millis(2_000))]
     connect_timeout: Duration,
-    #[builder(default = Arc::new(indicatif::MultiProgress::new()))]
-    multi_progress: Arc<indicatif::MultiProgress>,
-    #[builder(default = Arc::new(
-      reqwest::Client::builder()
-          .connect_timeout(Duration::from_millis(2_000))
-          .pool_max_idle_per_host(0)
-          .build()
-          .expect("Failed to create HTTP client")
-  ))]
-    client: Arc<reqwest::Client>,
 }
 
 impl DownloadProgress {
@@ -43,13 +33,20 @@ impl DownloadProgress {
     }
 
     pub async fn download(&self, downloads: Vec<(&str, &str)>) -> anyhow::Result<()> {
+        let client = reqwest::Client::builder()
+            .connect_timeout(self.connect_timeout)
+            .pool_max_idle_per_host(0)
+            .build()?;
+
+        let mp = indicatif::MultiProgress::new();
+
         let futures = downloads
             .into_iter()
-            .map(|(url, path)| self.inner_download(&url, &path));
+            .map(|(url, path)| self.inner_download(&client, &mp, &url, &path));
 
         futures::future::try_join_all(futures).await?;
-        self.multi_progress.set_move_cursor(true);
-        self.multi_progress.clear()?;
+        mp.set_move_cursor(true);
+        mp.clear()?;
         println!("🎉 Download completed");
 
         Ok(())
@@ -64,45 +61,60 @@ impl DownloadProgress {
             .unwrap()
             .progress_chars("━━"),
         );
-        self.multi_progress.add(progress_bar)
+        progress_bar
     }
 
-    async fn inner_download(&self, url: &str, path: &str) -> anyhow::Result<()> {
+    async fn send(
+        &self,
+        client: &reqwest::Client,
+        url: &str,
+        downloaded_size: u64,
+    ) -> Result<reqwest::Response, backoff::Error<anyhow::Error>> {
+        let request = client
+            .get(url)
+            .header("Range", format!("bytes={}-", downloaded_size));
+        let response = request.send().await.map_err(|e| {
+            if is_retry_error(&e) {
+                // 网络相关的临时错误，应该重试
+                backoff::Error::transient(anyhow::Error::from(e))
+            } else {
+                // 其他错误视为永久错误
+                backoff::Error::permanent(anyhow::Error::from(e))
+            }
+        })?;
+        if !response.status().is_success() {
+            return Err(backoff::Error::transient(anyhow::anyhow!(
+                "Get request failed, Http status code not ok {} : {:?}",
+                response.status(),
+                response
+            )));
+        }
+        Ok(response)
+    }
+
+    async fn inner_download(
+        &self,
+        client: &reqwest::Client,
+        mp: &indicatif::MultiProgress,
+        url: &str,
+        path: &str,
+    ) -> anyhow::Result<()> {
         let temp_dir = env::temp_dir();
         let temp_file = temp_dir.join(path);
 
         let progress_bar = self.prepare_progress_bar();
+        let progress_bar = mp.add(progress_bar);
 
         backoff::future::retry(self.backoff(), || async {
-            let mut downloaded_size = temp_file.metadata().map(|item| item.len()).unwrap_or(0);
+            let downloaded_size = temp_file.metadata().map(|item| item.len()).unwrap_or(0);
 
-            let request = self
-                .client
-                .get(url)
-                .header("Range", format!("bytes={}-", downloaded_size));
-
-            let response = request.send().await.map_err(|e| {
-                if is_retry_error(&e) {
-                    // 网络相关的临时错误，应该重试
-                    backoff::Error::transient(anyhow::Error::from(e))
-                } else {
-                    // 其他错误视为永久错误
-                    backoff::Error::permanent(anyhow::Error::from(e))
-                }
-            })?;
-
-            if !response.status().is_success() {
-                // return Err(backoff::Error::Permanent(anyhow::anyhow!(
-                //     "Get request failed, Http status code not ok {} : {:?}",
-                //     response.status(),
-                //     response
-                // )));
-                return Err(backoff::Error::transient(anyhow::anyhow!(
-                    "Get request failed, Http status code not ok {} : {:?}",
-                    response.status(),
-                    response
-                )));
-            }
+            let response = self
+                .send(client, url, downloaded_size)
+                .await
+                .map_err(|e| match e {
+                    backoff::Error::Permanent(e) => e,
+                    backoff::Error::Transient { err, .. } => err,
+                })?;
 
             let supports_resume = response.status() == reqwest::StatusCode::PARTIAL_CONTENT;
 
@@ -110,7 +122,11 @@ impl DownloadProgress {
 
             let total_size = remaining_size + downloaded_size;
 
-            let mut state = DownloadState::new(url.to_string(), downloaded_size, total_size);
+            let mut state = DownloadState::builder()
+                .downloaded_size(downloaded_size)
+                .remaining_size(remaining_size)
+                .url(url.to_string())
+                .build();
 
             let file = tokio::fs::OpenOptions::new()
                 .write(true)
@@ -142,10 +158,6 @@ impl DownloadProgress {
                     }
                 })?
             {
-                downloaded_size += chunk.len() as u64;
-                // progress_bar.set_position(downloaded_size);
-                // progress_bar.set_message(format!("{}", url));
-
                 state.update_progress(chunk.len(), &progress_bar);
 
                 writer
--- src/state.rs
+++ src/state.rs
@@ -1,25 +1,33 @@
 use indicatif::ProgressBar;
 use std::time::Instant;
+use typed_builder::TypedBuilder;
 
-#[derive(Debug)]
+#[derive(Debug, TypedBuilder)]
 pub struct DownloadState {
+    #[builder(default = 0)]
     downloaded_size: u64,
-    last_downloaded_size: u64,
+    #[builder(default = 0)]
+    remaining_size: u64,
+    // #[builder(default = 0)]
+    // last_downloaded_size: u64,
+    #[builder(default = Instant::now())]
     start_time: Instant,
+    #[builder(default = String::new())]
     url: String,
-    total_size: u64,
+    // #[builder(default = 0)]
+    // total_size: u64,
 }
 
 impl DownloadState {
-    pub fn new(url: String, initial_size: u64, total_size: u64) -> Self {
-        Self {
-            downloaded_size: initial_size,
-            last_downloaded_size: initial_size,
-            start_time: Instant::now(),
-            url,
-            total_size,
-        }
-    }
+    // pub fn new(url: String, initial_size: u64, total_size: u64) -> Self {
+    //     Self {
+    //         downloaded_size: initial_size,
+    //         last_downloaded_size: initial_size,
+    //         start_time: Instant::now(),
+    //         url,
+    //         total_size,
+    //     }
+    // }
 
     pub fn update_progress(&mut self, chunk_size: usize, progress_bar: &ProgressBar) {
         self.downloaded_size += chunk_size as u64;
@@ -30,13 +38,15 @@ impl DownloadState {
     fn update_speed(&mut self, progress_bar: &ProgressBar) {
         let elapsed = self.start_time.elapsed().as_secs_f64();
         if elapsed > 0.0 {
-            let speed = (self.downloaded_size - self.last_downloaded_size) as f64
-                / elapsed
-                / 1024.0
-                / 1024.0;
-            let percentage = (self.downloaded_size as f64 / self.total_size as f64 * 100.0) as u64;
-            progress_bar.set_message(format!("{} {:.2} MB/s {}%", self.url, speed, percentage));
-            self.last_downloaded_size = self.downloaded_size;
+            // let speed = (self.downloaded_size - self.last_downloaded_size) as f64
+            //     / elapsed
+            //     / 1024.0
+            //     / 1024.0;
+            let percentage = (self.downloaded_size as f64
+                / (self.remaining_size + self.downloaded_size) as f64
+                * 100.0) as u64;
+            progress_bar.set_message(format!("{}% {} ", percentage, self.url));
+            // self.last_downloaded_size = self.downloaded_size;
         }
     }
 }
